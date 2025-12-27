041b89886414f064575891d2f104bafaaa09826d
ladraa selma <ledraselma@gmail.com>
2025-12-27 19:26:37 +0100

    Add gitignore and complete TP source code

--- MyGitApp/src/main/java/com/l3/gl/model/User.java
+++ MyGitApp/src/main/java/com/l3/gl/model/User.java
@@ -4,13 +4,11 @@ public class User {
     private String name;
     private int age;
 
-    // Constructor جديد
     public User(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Getters و Setters
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
 
